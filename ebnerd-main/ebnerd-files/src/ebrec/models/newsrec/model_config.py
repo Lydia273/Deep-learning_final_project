31a9e74383f8948a6158823c842fdf962635cdbd
AntoniosOtapasidis <s232996@dtu.dk>
2024-12-20 11:57:11 +0100

    model config

--- ebnerd-main/ebnerd-files/src/ebrec/models/newsrec/model_config.py
+++ ebnerd-main/ebnerd-files/src/ebrec/models/newsrec/model_config.py
@@ -20,63 +20,8 @@ def hparams_to_dict(hparams_class) -> dict:
     return params
 
 
-class hparams_naml:
-    # INPUT DIMENTIONS:
-    title_size: int = DEFAULT_TITLE_SIZE
-    history_size: int = 20
-    body_size: int = DEFAULT_BODY_SIZE
-    vert_num: int = 100
-    vert_emb_dim: int = 10
-    subvert_num: int = 100
-    subvert_emb_dim: int = 10
-    # MODEL ARCHITECTURE
-    dense_activation: str = "relu"
-    cnn_activation: str = "relu"
-    attention_hidden_dim: int = 200
-    filter_num: int = 400
-    window_size: int = 3
-    # MODEL OPTIMIZER:
-    optimizer: str = "adam"
-    loss: str = "cross_entropy_loss"
-    dropout: float = 0.2
-    learning_rate: float = 1e-4
 
 
-class hparams_lstur:
-    # INPUT DIMENTIONS:
-    title_size: int = DEFAULT_TITLE_SIZE
-    history_size: int = 20
-    n_users: int = 50000
-    # MODEL ARCHITECTURE
-    cnn_activation: str = "relu"
-    type: str = "ini"
-    attention_hidden_dim: int = 200
-    gru_unit: int = 400
-    filter_num: int = 400
-    window_size: int = 3
-    # MODEL OPTIMIZER:
-    optimizer: str = "adam"
-    loss: str = "cross_entropy_loss"
-    dropout: float = 0.2
-    learning_rate: float = 1e-4
-
-
-class hparams_npa:
-    # INPUT DIMENTIONS:
-    title_size: int = DEFAULT_TITLE_SIZE
-    history_size: int = 20
-    n_users: int = 50000
-    # MODEL ARCHITECTURE
-    cnn_activation: str = "relu"
-    attention_hidden_dim: int = 200
-    user_emb_dim: int = 400
-    filter_num: int = 400
-    window_size: int = 3
-    # MODEL OPTIMIZER:
-    optimizer: str = "adam"
-    loss: str = "cross_entropy_loss"
-    dropout: float = 0.2
-    learning_rate: float = 1e-4
 
 
 class hparams_nrms:
@@ -97,18 +42,3 @@ class hparams_nrms:
     newsencoder_l2_regularization: float = 1e-4
 
 
-class hparams_nrms_docvec:
-    # INPUT DIMENTIONS:
-    title_size: int = DEFAULT_DOCUMENT_SIZE
-    history_size: int = 20
-    # MODEL ARCHITECTURE
-    head_num: int = 16
-    head_dim: int = 16
-    attention_hidden_dim: int = 200
-    # MODEL OPTIMIZER:
-    optimizer: str = "adam"
-    loss: str = "cross_entropy_loss"
-    dropout: float = 0.2
-    learning_rate: float = 1e-4
-    newsencoder_units_per_layer: list[int] = [512, 512, 512]
-    newsencoder_l2_regularization: float = 1e-4
